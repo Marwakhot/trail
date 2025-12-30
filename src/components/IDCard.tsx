6162d4eb01931e342b45fdbf7e619bd910e6397a
Marwakhot <marwakhot007@gmail.com>
2025-12-30 21:51:58 +0400

    add

--- src/components/IDCard.tsx
+++ src/components/IDCard.tsx
@@ -10,7 +10,7 @@ const IDCard = () => {
       {/* Card */}
       <div className="id-card-hang w-72 bg-card rounded-2xl shadow-2xl relative">
         {/* Photo - positioned above everything */}
-         <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/3 z-10">
+         <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-2 z-10">
           <div className="w-28 h-28 rounded-full border-4 border-white shadow-lg overflow-hidden bg-gradient-to-br from-primary/20 to-accent/20">
             <img 
               src="/profile.jpeg" 
